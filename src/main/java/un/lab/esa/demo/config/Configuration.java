aa9afb54b5defa694d7905eeca7ee3b847a3cafd
matyusha <i.matiushkina@gmail.com>
2021-01-17 15:00:32 +0400

    lab4

--- src/main/java/un/lab/esa/demo/config/Configuration.java
+++ src/main/java/un/lab/esa/demo/config/Configuration.java
@@ -13,7 +13,6 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
-
 import javax.jms.ConnectionFactory;
 
 @org.springframework.context.annotation.Configuration
@@ -37,13 +36,10 @@ public class Configuration extends WebMvcConfigurerAdapter {
     public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
         configurer.configure(factory, connectionFactory);
-        // You could still override some of Boot's default if necessary.
         return factory;
     }
 
-    // Serialize message content to json using TextMessage
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
--- src/main/java/un/lab/esa/demo/repository/AuthorRepository.java
+++ src/main/java/un/lab/esa/demo/repository/AuthorRepository.java
@@ -1,7 +1,9 @@
 package un.lab.esa.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import un.lab.esa.demo.model.Author;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
 }
